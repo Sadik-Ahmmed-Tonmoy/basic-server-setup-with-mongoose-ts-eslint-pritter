aae55bd9fb1accbe1038f65bf6c7b56c0e4640b1
Sadik Ahmmed Tonmoy <Sadikahmed2258@gmail.com>
2024-08-16 12:55:57 +0600

    user validation update

---
+++ src/app/modules/user/user.validation.ts
@@ -0,0 +1,35 @@
+import { z } from 'zod';
+
+const userValidationSchema = z.object({
+  userId: z.string().nonempty(),
+  name: z.object({
+    firstName: z.string().nonempty(),
+    lastName: z.string().nonempty(),
+  }),
+  // email: z
+  // .string({
+  //     required_error: "Email is required",
+  // })
+  // .regex(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Please enter a valid email address"),
+  email: z.string({ message: 'Email is required' }).email(),
+  phone: z.string().nonempty(),
+  password: z
+    .string()
+    .nonempty()
+    .max(20, { message: 'Password can not be more than 20 characters' }),
+  needChangePassword: z.boolean().optional().default(true),
+  avatar: z.string().optional(),
+  role: z.enum(['superAdmin', 'admin', 'user']),
+  status: z.enum(['active', 'inactive']).default('active'),
+  address: z
+    .object({
+      city: z.string().nonempty(),
+      zone: z.string().nonempty(),
+      area: z.string().nonempty(),
+      detailsAddress: z.string().nonempty(),
+    })
+    .optional(),
+  isDeleted: z.boolean().optional().default(false),
+});
+
+export const UserValidation = { userValidationSchema };
