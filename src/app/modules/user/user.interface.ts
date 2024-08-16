bac3a29b23bf0056d475afa97dc59b4fb3d83ed5
Sadik Ahmmed Tonmoy <Sadikahmed2258@gmail.com>
2024-08-16 12:27:46 +0600

    Schema update

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -3,8 +3,23 @@ export type TUserName = {
   lastName: string;
 };
 
+export type TAddress = {
+  city: string;
+  zone: string;
+  area: string;
+  detailsAddress: string;
+};
+
 export type TUser = {
+  userId: string;
   name: TUserName;
   email: string;
+  phone: string;
+  password: string;
+  needChangePassword: boolean;
   avatar?: string;
+  role: 'superAdmin' | 'admin' | 'user';
+  status: 'active' | 'inactive';
+  address?: TAddress;
+  isDeleted: boolean;
 };
--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,5 +1,5 @@
 import { Schema, model } from 'mongoose';
-import { TUser, TUserName } from './user.interface';
+import { TAddress, TUser, TUserName } from './user.interface';
 
 const userNameSchema = new Schema<TUserName>({
     firstName: {
@@ -16,13 +16,80 @@ const userNameSchema = new Schema<TUserName>({
     },
   });
 
+ const addressSchema = new Schema<TAddress>({
+  city: {
+    type: String,
+    required: [true, 'City is required'],
+  },
+  zone: {
+    type: String,
+    required: [true, 'Zone is required'],
+  },
+  area: {
+    type: String,
+    required: [true, 'Area is required'],
+  },
+  detailsAddress: {
+    type: String,
+    required: [true, 'Details Address is required'],
+  },
+ })
+
+
 const userSchema = new Schema<TUser>({
+  userId: {
+    type: String,
+    required: true,
+    unique: true, 
+  },
   name: {
     type: userNameSchema,
     required: [true, 'Name is required'],
   },
-  email: { type: String, required: true },
-  avatar: { type: String, required: false },
+  email: {
+    type: String,
+    required:[ true, 'Email is required'],
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  phone: {
+    type: String,
+    required: [true, 'Phone is required'],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minlength: [6, 'Password must be at least 6 characters']
+  },
+  needChangePassword: {
+    type: Boolean,
+    default: true,
+  },
+  avatar: {
+    type: String,
+    required: false,
+  },
+  role: {
+    type: String,
+    enum: ['superAdmin', 'admin', 'user'],
+    default: 'user',
+  },
+  status: {
+    type: String,
+    enum: ['active', 'inactive'],
+    default: 'active',
+  },
+  address: {
+    type: addressSchema,
+    required: false,
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 const User = model<TUser>('User', userSchema);
+ 
